12038fcf269310c0f11f5ccf9e7ebf900e830150
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-23 22:15:02 -0300

    guardando

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <header className="bg-lime-600 py-5">
+        <div className="max-w-3xl mx-auto flex justify-between">
+          <h1 className="text-center font-bold text-3xl uppercase text-white">Contador de Calorias</h1>
+        </div>
+      </header>
+
+      <section></section>
     </> 
   )
 }
