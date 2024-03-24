fd96b5332a454fea9b155010f963b07e14b3a49c
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-24 17:03:14 -0300

    Agregando el Formulario de Entrada de datos

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,52 @@
+import { categories } from "../data/categories"
 
 const Form = () => {
 
   return (
-    <form className="rounded-lg shadow space-y-5 bg-white p-10"></form>
+    <form className="rounded-lg shadow space-y-5 bg-white p-10">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">Categoría:</label>
+        <select
+          name="category"
+          id="category"
+          className="border-2 border-slate-200 p-2 rounded-lg"
+        >
+          {
+            categories.map(category => (
+              <option key={category.id} value={category.id}>{category.name}</option>
+            ))
+          }
+        </select>
+      </div>
+      
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">Actividad:</label>
+        <input
+          type="text"
+          name="activity"
+          id="activity" 
+          className="border-2 border-slate-200 p-2 rounded-lg"
+          placeholder="Ej: manzana, jugo de naranja, ensaldada, bicicleta"
+        />
+      </div>
+      
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">Calorías:</label>
+        <input
+          type="text"
+          name="calories"
+          id="calories" 
+          className="border-2 border-slate-200 p-2 rounded-lg"
+          placeholder="Ej: 350 o 500 calorías"
+        />
+      </div>
+
+      <input
+        type="submit"
+        value="Agregar" 
+        className="bg-gray-800 w-full py-2 text-white uppercase text-lg font-bold cursor-pointer rounded-lg"
+      />
+    </form>
   )
 }
 
