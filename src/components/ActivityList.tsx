5d2f6ef465663e621399c5bb466d30c17b5a5183
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-25 16:27:54 -0300

    Añadiendo más información al listado de Actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,10 +1,16 @@
+import { useMemo } from "react"
+import { categories } from "../data/categories"
 import { Activity } from "../types"
+
 
 type ActivityListProps = {
   activities: Activity[]
 }
 
 const ActivityList = ({ activities } : ActivityListProps) => {
+
+  const categoryName = useMemo(
+    () => (category : Activity["category"]) => categories.map(cat => cat.id === category ? cat.name : ""), [activities])
 
   return (
     <>
@@ -16,7 +22,7 @@ const ActivityList = ({ activities } : ActivityListProps) => {
             className="px-5 py-10 bg-white mt-5 flex justify-between"
           >
             <div className="space-y-2 relative">
-              <p>{activity.category}</p>
+              <p className={`absolute -top-8 -left-8 px-5 py-2 text-white uppercase font-bold ${activity.category === 1 ? `bg-lime-500` : `bg-orange-500`}`}>{categoryName(activity.category)}</p>
               <p className="text-3xl font-bold capitalize">{activity.name}</p>
               <p className="text-4xl font-black text-lime-500">{activity.calories} Calorías</p>
             </div>
