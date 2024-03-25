b23a707c9fa9acc22a00354d3b9055b87ba45754
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-25 17:08:30 -0300

    Almacenando el ID a editar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,13 +1,15 @@
-import { useMemo } from "react"
+import { Dispatch, useMemo } from "react"
 import { categories } from "../data/categories"
 import { Activity } from "../types"
 import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline'
+import { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
   activities: Activity[]
+  dispatch: Dispatch<ActivityActions>
 }
 
-const ActivityList = ({ activities } : ActivityListProps) => {
+const ActivityList = ({ activities, dispatch } : ActivityListProps) => {
 
   const categoryName = useMemo(
     () => (category : Activity["category"]) => categories.map(cat => cat.id === category ? cat.name : ""), [activities])
@@ -21,14 +23,18 @@ const ActivityList = ({ activities } : ActivityListProps) => {
             key={activity.id}
             className="px-5 py-10 bg-white mt-5 flex justify-between"
           >
-            <div className="space-y-2 relative">
+            <div className="space-y-4 relative">
               <p className={`absolute -top-8 -left-8 px-5 py-2 text-white uppercase font-bold ${activity.category === 1 ? `bg-lime-500` : `bg-orange-500`}`}>{categoryName(activity.category)}</p>
               <p className="text-3xl font-bold capitalize">{activity.name}</p>
               <p className="text-4xl font-black text-lime-500">{activity.calories} Calorías</p>
             </div>
 
             <div className="flex gap-5 items-center">
-              <button>
+              <button onClick={() => dispatch({
+                  type: "set-activeId",
+                  payload: { id: activity.id }
+                })}
+              >
                 <PencilSquareIcon 
                   className="h-8 w-8 text-gray-800"
                 />
